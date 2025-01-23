db6c8f982211310b9a9b5b4f53f61037a04328d2
calinpj04 <calinjohnson0815@gmail.com>
2025-01-23 01:57:23 -0500

    pls work

---
+++ application1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    print(request)
+    return HttpResponse("hello girlie")
+
+def home2(request):
+    return render(request, "home.html")
