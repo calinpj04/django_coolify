36e37c7aac4efdd265792e8702513ac9c9f8015e
calinpj04 <calinjohnson0815@gmail.com>
2025-01-23 02:58:30 -0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -132,3 +132,5 @@ WHITENOISE_USE_FINDERS = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_COOKIE_SECURE = False
