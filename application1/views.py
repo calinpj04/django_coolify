a83f58c42c66907104b1b5d6b31c9e2073e298f4
calinpj04 <calinjohnson0815@gmail.com>
2025-01-23 03:29:42 -0500

    ee

--- application1/views.py
+++ application1/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
@@ -8,5 +10,7 @@ def home(request):
 def home2(request):
     return render(request, "home.html")
 
+@csrf_exempt
 def kofi(request):
      print(request)
+     return JsonResponse()
