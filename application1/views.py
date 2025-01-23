74a8f17db234726c775f250917b865244511b5fd
calinpj04 <calinjohnson0815@gmail.com>
2025-01-23 03:42:38 -0500

    eek

--- application1/views.py
+++ application1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from discord import Webhook, RequestsWebhookAdapter
 
 # Create your views here.
 def home(request):
@@ -13,4 +14,6 @@ def home2(request):
 @csrf_exempt
 def kofi(request):
      print(request)
+     webhook = Webhook.from_url("https://discord.com/api/webhooks/1331777737479946290/QhAnInypymU_xMpNcXI124FHJ8iI_hKnBk7zarwcL1y3omGU8w6EDK1XRjjbUsh8brJj", adapter=RequestsWebhookAdapter())
+     webhook.send("Hello")
      return JsonResponse({})
