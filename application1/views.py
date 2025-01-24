c3142408cacfb3f585112c9a88b43e690d6e146e
calinpj04 <calinjohnson0815@gmail.com>
2025-01-23 22:03:31 -0500

    Update views.py

--- application1/views.py
+++ application1/views.py
@@ -13,8 +13,10 @@ def home2(request):
     return render(request, "home.html")
 
 @csrf_exempt
-def kofi(request):    
+def kofi(request):   
+    form_data = json.loads(request.body)
+    print(form_data)
     webhook = SyncWebhook.from_url('https://discord.com/api/webhooks/1331777737479946290/QhAnInypymU_xMpNcXI124FHJ8iI_hKnBk7zarwcL1y3omGU8w6EDK1XRjjbUsh8brJj') # Initializing webhook
-    webhook.send(content=request) # Executing webhook.
+    webhook.send(content=form_data) # Executing webhook.
 
     return JsonResponse({})
