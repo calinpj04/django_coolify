9c358c813ea71d8453cb786e0ff7bf123216d5f7
calinpj04 <calinjohnson0815@gmail.com>
2025-01-24 22:22:00 -0500

    Update views.py

--- application1/views.py
+++ application1/views.py
@@ -1,8 +1,10 @@
+import json
+import uuid
+
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from discord import SyncWebhook
-import json
 
 # Create your views here.
 def home(request):
@@ -13,12 +15,19 @@ def home2(request):
     return render(request, "home.html")
 
 def kofiemail(request):
+    uuid2 = uuid.uuid4()
+    uuid_str = str(uuid2)
+    print(uuid_str+"Test")
     return render(request, "KofiPurchaseEmail.html")
 
 @csrf_exempt
 def kofi(request):   
     form_data = request.POST['data']
-
+    if form_data["verification_token"] != "4afd662e-6787-4010-9783-4329d70a36f3":
+        return ("Invalid token", 403)
+    else:
+        print("IS VAILD")
+    
     print(form_data)
 
     webhook = SyncWebhook.from_url('https://discord.com/api/webhooks/1331777737479946290/QhAnInypymU_xMpNcXI124FHJ8iI_hKnBk7zarwcL1y3omGU8w6EDK1XRjjbUsh8brJj') # Initializing webhook
